633f863b5c3e930b0b4489526e5e364dd1a334be
Harry <staleyh@gmail.com>
2019-03-21 00:58:10 -0500

    Stuff

--- schedule_fcfs.c
+++ schedule_fcfs.c
@@ -1,14 +1,28 @@
 //
 // Created by  Harry Staley on 3/20/19.
 //
+#include <stdlib.h>
+#include <zconf.h>
 #include "schedulers.h"
+#include "list.h"
+#include "task.h"
 
 #define MIN_PRIORITY 1
 #define MAX_PRIORITY 10
 
 // add a task to the list
 void add(char *name, int priority, int burst){
-
+/*
+    int tid = getpid();
+    struct node head;
+    struct task newTask = {
+        name,
+        tid,
+        priority,
+        burst
+    };
+    insert(&head, &newTask);
+*/
 }
 
 // invoke the scheduler
