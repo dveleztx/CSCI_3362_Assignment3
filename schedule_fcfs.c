eee4b987f32da2dbbce071f13af2795f15170fc6
dveleztx <dveleztx@gmail.com>
2019-03-24 01:54:36 -0500

    Created Task and Node -DV

--- schedule_fcfs.c
+++ schedule_fcfs.c
@@ -8,31 +8,41 @@
 #include "task.h"
 #include "list.h"
 #include "schedulers.h"
-#include "list.h"
-#include "task.h"
 
 #define MIN_PRIORITY 1
 #define MAX_PRIORITY 10
 #define TID 1
 
-Task *sendTask = NULL;
-struct node *myNode = NULL;
-
-
 void add(char *name, int priority, int burst) {
 
+	// Create task
 	Task myTask = { name, TID, priority, burst };
-	sendTask = &myTask;
+	//printf("Name: %s\tPriority: %d\tBurst: %d\n", myTask.name, myTask.priority, myTask.burst);
+
+	// Create Node
+	struct node *head = NULL;
+	head = malloc(sizeof(struct node));
+
+	if (head == NULL) {
+		perror("error");
+	}
+
+	head->task = &myTask;
+	head->next = NULL;
+
+	while (head != NULL) {
+
+		printf("Name: %s\tPriority: %d\tBurst: %d\n", head->task->name, head->task->priority, head->task->burst);
+		head = head->next;
+	}
+
+	//insert(head, myTask);
+
+	/*sendTask = &myTask;
 
 	struct node next;
 	struct node a = { sendTask, &next };
-	myNode = &a;
-
-	printf("Name: %s\tPriority: %d\tBurst: %d\n", myTask.name, myTask.priority, myTask.burst);
-
-	insert(&myNode, sendTask);
-
-	schedule();
+	myNode = &a;*/
 
 }
 
